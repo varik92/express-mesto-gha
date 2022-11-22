58d277c72e707ddf12ccf9d5bc7a83977937f4f0
Vartan Davtyan <vrtndvtn@yandex.ru>
2022-11-22 23:43:50 +0300

    14-1

--- routes/auth.js
+++ routes/auth.js
@@ -17,7 +17,7 @@ router.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(/https?:\/\/w{0,3}\.?[\S]+/gmi),
+    avatar: Joi.string().pattern(/https?:\/\/w{0,3}\.?[\S]+/),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
