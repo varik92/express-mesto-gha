76c5abfbe5df2948c4efa4883aa169ba2799e2ed
Vartan Davtyan <vrtndvtn@yandex.ru>
2022-12-04 18:12:49 +0300

    подготовка к 15

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
@@ -23,7 +24,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(requestLogger);
 
-app.use(corsHandler);
+app.use(cors(corsHandler));
 
 app.use('/', routerAuth);
 
